85c13ad7e5d07f2a68df621043b6fa804c0f2db4
JonathanBenitezh1 <jonathanbenitezh1@gmail.com>
2023-03-02 18:27:27 -0300

    pagina header

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoapComponent } from './componentes/logoap/logoap.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BanerComponent } from './componentes/baner/baner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    BanerComponent
   ],
   imports: [
     BrowserModule,
